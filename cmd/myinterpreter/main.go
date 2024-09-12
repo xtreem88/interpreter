b7f9847bec15326adc44e6bd151e218ef31c6029
Segun <folowosele.segun@gmail.com>
2024-09-12 12:55:41 +0400

    added scan

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/codecrafters-io/interpreter-starter-go/cmd/scanner"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
 		os.Exit(1)
@@ -21,8 +20,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Uncomment this block to pass the first stage
-	//
 	filename := os.Args[2]
 	fileContents, err := os.ReadFile(filename)
 	if err != nil {
@@ -30,9 +27,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
-	} else {
-		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
+	scanner := scanner.NewScanner(string(fileContents))
+	tokens := scanner.ScanTokens()
+
+	for _, token := range tokens {
+		fmt.Printf("%s %s null\n", token.Type, token.Lexeme)
 	}
 }
---
+++ cmd/scanner/scanner.go
@@ -0,0 +1,60 @@
+package scanner
+
+type TokenType string
+
+const (
+	LEFT_PAREN  TokenType = "LEFT_PAREN"
+	RIGHT_PAREN TokenType = "RIGHT_PAREN"
+	EOF         TokenType = "EOF"
+)
+
+type Token struct {
+	Type    TokenType
+	Lexeme  string
+	Literal interface{}
+}
+
+type Scanner struct {
+	source  string
+	tokens  []Token
+	start   int
+	current int
+}
+
+func NewScanner(source string) *Scanner {
+	return &Scanner{source: source}
+}
+
+func (s *Scanner) ScanTokens() []Token {
+	for !s.isAtEnd() {
+		s.start = s.current
+		s.scanToken()
+	}
+
+	s.tokens = append(s.tokens, Token{Type: EOF, Lexeme: "", Literal: nil})
+	return s.tokens
+}
+
+func (s *Scanner) scanToken() {
+	c := s.advance()
+	switch c {
+	case '(':
+		s.addToken(LEFT_PAREN)
+	case ')':
+		s.addToken(RIGHT_PAREN)
+	}
+}
+
+func (s *Scanner) advance() byte {
+	s.current++
+	return s.source[s.current-1]
+}
+
+func (s *Scanner) addToken(tokenType TokenType) {
+	text := s.source[s.start:s.current]
+	s.tokens = append(s.tokens, Token{Type: tokenType, Lexeme: text, Literal: nil})
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
+}
